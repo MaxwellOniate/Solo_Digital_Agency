269fc2347c58e6a3e7ef0bf42ca5df3e36dcbc63
maxwelloniate <maxwelloniate@gmail.com>
2020-01-04 10:09:25 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,197 @@
+// Remove preloader once page is fully loaded.
+$(window).on('load', function() {
+  $('#status').fadeOut();
+  $('#preloader')
+    .delay(350)
+    .fadeOut('slow');
+});
+
+// Show & Hide White Navigation Bar
+$(function() {
+  showHideNav();
+
+  $(window).scroll(function() {
+    showHideNav();
+  });
+
+  function showHideNav() {
+    if ($(window).scrollTop() > 50) {
+      // Show white nav
+      $('nav').addClass('white-nav');
+      // Show dark logo
+      $('.navbar-brand img').attr('src', 'img/logo/logo-dark.png');
+    } else {
+      // Hide white nav
+      $('nav').removeClass('white-nav');
+      // Show white logo
+      $('.navbar-brand img').attr('src', 'img/logo/logo.png');
+    }
+  }
+});
+
+// Init scrollspy
+$('body').scrollspy({ target: '.navbar' });
+
+// Add smooth scrolling
+$('#main-nav a').on('click', function(e) {
+  // Check for a hash value.
+  if (this.hash !== '') {
+    // Prevent default behaviors.
+    e.preventDefault();
+    // Store hash.
+    const hash = this.hash;
+    // Animate smooth scroll.
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      900
+    );
+  }
+});
+// Add smooth scrolling
+$('#main-nav a, #home-content a').on('click', function(e) {
+  // Check for a hash value.
+  if (this.hash !== '') {
+    // Prevent default behaviors.
+    e.preventDefault();
+    // Store hash.
+    const hash = this.hash;
+    // Animate smooth scroll.
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      900
+    );
+  }
+});
+
+// Team Owl Carousel Slider
+$(function() {
+  $('#team-members').owlCarousel({
+    items: 2,
+    autoplay: true,
+    smartSpeed: 700,
+    loop: true,
+    autoplayHoverPause: true,
+    nav: true,
+    dots: false
+  });
+});
+
+// Team Progress Bars
+$(function() {
+  $('#progress-bars').waypoint(
+    function() {
+      $('.progress-bar').each(function() {
+        $(this).animate(
+          {
+            width: $(this).attr('aria-valuenow') + '%'
+          },
+          2000
+        );
+      });
+      this.destroy();
+    },
+    { offset: 'bottom-in-view' }
+  );
+});
+
+// Portfolio Filter
+$(window).on('load', function() {
+  // Init Isotope
+  $('#isotope-container').isotope({});
+  // Filter items on button click
+  $('#isotope-filters').on('click', 'button', function() {
+    // Get filter value
+    const filterValue = $(this).attr('data-filter');
+    // Filter portfolio items
+    $('#isotope-container').isotope({
+      filter: filterValue
+    });
+    // Active button
+    $('#isotope-filters')
+      .find('.active')
+      .removeClass('active');
+    $(this).addClass('active');
+  });
+});
+
+// Magnific Popup
+$(function() {
+  $('#isotope-container').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery: {
+      enabled: true
+    }
+  });
+});
+
+// Testimonial Slider
+$(function() {
+  $('#testimonial-slider').owlCarousel({
+    items: 1,
+    autoplay: true,
+    smartSpeed: 700,
+    loop: true,
+    autoplayHoverPause: true,
+    nav: true,
+    dots: false
+  });
+});
+
+// Stats Count Up
+
+$(function() {
+  $('.counter').counterUp({
+    delay: 10,
+    time: 2000
+  });
+});
+
+// Clients
+
+$(function() {
+  $('#client-list').owlCarousel({
+    items: 6,
+    autoplay: false,
+    smartSpeed: 700,
+    loop: true,
+    autoplayHoverPause: true,
+    nav: true,
+    dots: false
+  });
+});
+
+// Google Map
+
+$(window).on('load', function() {
+  // Map Variables
+  const addressString = '230 Broadway, NY, New York 10007, USA';
+  const myLatLng = { lat: 40.712685, lng: -74.00592 };
+
+  // Render Map
+  let map = new google.maps.Map(document.getElementById('map'), {
+    center: myLatLng,
+    zoom: 11
+  });
+
+  // Add Marker
+  const marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Click To See Address'
+  });
+
+  // Add Info Window
+  const infoWindow = new google.maps.InfoWindow({
+    content: addressString
+  });
+
+  // Show info window on marker click
+  marker.addListener('click', function() {
+    infoWindow.open(map, marker);
+  });
+});
